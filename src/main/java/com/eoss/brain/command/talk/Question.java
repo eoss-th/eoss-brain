a81df6734c361f3a39579a18fa275562ca465c93
eoss-th <wisarut.sri@eoss-th.com>
2019-11-28 17:30:30 +0700

    v1.3
    Support Link to App

--- src/main/java/com/eoss/brain/command/talk/Question.java
+++ src/main/java/com/eoss/brain/command/talk/Question.java
@@ -107,7 +107,7 @@ public class Question {
 
                 String linkURL = responses[responses.length-1].trim();
 
-                if (!linkURL.startsWith("https://") && !linkURL.startsWith("tel:") && !linkURL.startsWith("mailto:")) {
+                if ( (!linkURL.startsWith("https://") || !linkURL.contains("://") /*For App*/) && !linkURL.startsWith("tel:") && !linkURL.startsWith("mailto:")) {
                     linkURL = null;
                 }
 
